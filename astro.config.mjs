66b6095a004a53b54bdda20f2ca170f8a0913309
Ori Naveh <orinaveh1@gmail.com>
2023-03-24 22:39:25 +0300

    ci: deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,5 +14,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://orinaveh.github.io',
   integrations: [mdx(), tailwind(), react(), svelte()]
 });
