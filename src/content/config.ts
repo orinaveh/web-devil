6224792e66b7f066347e11cf5a730f5b1fe16558
Ori Naveh <orinaveh1@gmail.com>
2023-03-24 22:32:40 +0300

    refactor: use content folder

---
+++ src/content/config.ts
@@ -0,0 +1,22 @@
+import { z, defineCollection } from 'astro:content';
+
+function parseDMY(value: string) {
+  const date = value.split("-");
+  const d = parseInt(date[0], 10),
+      m = parseInt(date[1], 10),
+      y = parseInt(date[2], 10);
+  return new Date(y, m - 1, d);
+}
+
+const articlesCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    author: z.string(),
+    publishDate: z.string().transform((str) => parseDMY(str)),
+  }),
+});
+
+export const collections = {
+  'articles': articlesCollection,
+};
