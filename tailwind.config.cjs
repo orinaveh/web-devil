6cec478ac30eabeb9ab2e4b018310ab51fe7121a
Ori Naveh <orinaveh1@gmail.com>
2023-03-24 22:08:36 +0300

    style: on build notice

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+		animation: {
+			shift: 'shift 3s linear infinite',
+		  },
+		  keyframes: {
+			shift: {
+			  '0%': { backgroundColor: 'white' },
+			  '50%': { backgroundColor: colors.red[600] },
+			  '100%': { backgroundColor: 'white' },
+			}
+		  }},
 	},
 	plugins: [],
 }
