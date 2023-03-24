273cf01fb4ae10cfff2a9237b419c9bee170e70e
Ori Naveh <orinaveh1@gmail.com>
2023-03-24 22:57:22 +0300

    fix: add base path for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,5 +15,6 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://orinaveh.github.io',
+  base: '/web-devil',
   integrations: [mdx(), tailwind(), react(), svelte()]
 });
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { MdMenu } from 'react-icons/md/index.js';
 
 export default () => (
 <header className="p-8 bg-black flex justify-between items-center">
-<a href="/" className="uppercase text-white text-2xl font-bold">Web Dev<span className="opacity-50 text-red-600">.</span>il</a>
+<a href="/web-devil" className="uppercase text-white text-2xl font-bold">Web Dev<span className="opacity-50 text-red-600">.</span>il</a>
 <button className="bg-white p-2"><MdMenu /></button>
 </header>
 )
